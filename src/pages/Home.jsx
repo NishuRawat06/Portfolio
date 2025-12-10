3630f39bf9adb5891fb4810fdda389d636079064
NishuRawat06 <rawatnishu2005@gmail.com>
2025-12-10 20:59:30 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import Footer from "../components/Footer";
-import Intro from "../components/intro";
+import Footer from "../components/Footer.jsx";
+import Intro from "../components/intro.jsx";
 export default function Home(){
     return(
         <div className="min-h-screen flex flex-col justify-evenly">
